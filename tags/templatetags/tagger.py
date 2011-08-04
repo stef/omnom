b17825375015733df7ca328999356e235e43d7af
stef <stefan.marsiske@gmail.com>
2011-08-04 21:53:21 +0200

    [enh] added filter for shorturls

--- tags/templatetags/tagger.py
+++ tags/templatetags/tagger.py
@@ -1,6 +1,7 @@
 from django.template import Library, Variable
 from django.conf import settings
 from django import template
+from tags.baseconv import base62
 import random
 
 register = Library()
@@ -13,3 +14,7 @@ def root_url():
 def media_url():
     return settings.MEDIA_URL
 
+
+@register.filter
+def shorturl(value):
+    return base62.from_decimal(value)
